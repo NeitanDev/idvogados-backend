8a55660e5539257c4a3b6fe4b97fcf99c99fd1ff
NeitanDev <natan.miranda02@gmail.com>
2020-04-28 12:41:03 -0300

    connection com db

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+    dialect: 'mysql',
+    host: 'db4free.net',
+    username: 'kairos_consult',
+    password: 'sounousounou',
+    database: 'db_idvogados',
+    define: {
+        timestamps: true,
+        underscored: true,
+    }
+};
